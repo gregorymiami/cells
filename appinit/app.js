43dd41ee9bc9ac15b60c59d7978f1f5de66757b2
gregorymiami <gregory.from.miami@gmail.com>
2021-03-14 22:52:43 -0700

    adds rock paper scissors

--- appinit/app.js
+++ appinit/app.js
@@ -11,7 +11,6 @@ let { width, height } = canvas.getBoundingClientRect();
 let cell_dims = 10;
 let rows = height / cell_dims;
 let columns = width / cell_dims;
-let grid_data = {}
 console.log(`width: ${width} and height: ${height}`);
 
 let convertToHtmlX = (x) => {
@@ -29,7 +28,6 @@ let drawCircle = (x, y, color, radius) => {
   context.beginPath();
   context.ellipse(realX, realY, radius, radius, 0, 0, Math.PI * 2);
   context.stroke();
-  console.log("done");
 }
 
 let clearCanvas = () => {
@@ -61,30 +59,106 @@ let drawCellWall = (row, column, color) => {
   drawRect(y, x, color, cell_dims, cell_dims); 
 }
 
-let drawGrid = () => {
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= columns; j++) {
+let drawGrid = (algo) => {
+  for (let i = 1; i <= algo.rows; i++) {
+    for (let j = 1; j <= algo.columns; j++) {
       drawCellWall(i, j, "black");
     }
   }
 }
 
-let setCell = (row, column, color) => {
-  grid_data[`${row}:${column}`] = "black";
-}
-
-let unsetCell = (row, column) => {
-  delete grid_data[`${row}:${column}`];
-}
-
-let drawAutomota = () => {
+let drawAutomota = (grid_data) => {
   for (const [key, value] of Object.entries(grid_data)) {
     let coordinates = key.split(':');
     drawCell(parseInt(coordinates[0]), parseInt(coordinates[1]), value);
   }  
 }
 
+class RockPaperScissors {
+  getAlgo(rows, columns, initialConditions, runs) {
+    let algo = {};
+    algo["rows"] = rows;
+    algo["columns"] = columns;
+    algo["grid"] = {};
+    algo["runs"] = runs;
+    for (let index in initialConditions) {
+      algo.grid[initialConditions[index].coordinates] = initialConditions[index].color;
+    }
+    
+    return algo;
+  }
+
+  // Returns true if the current_color is beaten
+  rockPaperScissors(current_color, neighbor_color) {
+    return (current_color === "blue" && neighbor_color === "red") || (current_color === "green" && neighbor_color === "blue") || (current_color === "red" && neighbor_color === "green");
+  }
+
+  checkNeighbors(row, column, grid) {
+    let current_color = grid[`${row}:${column}`];
+
+    if (this.rockPaperScissors(current_color, grid[`${row - 1}:${column - 1}`])) {
+      return grid[`${row - 1}:${column - 1}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row - 1}:${column}`])) {
+      return grid[`${row - 1}:${column}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row - 1}:${column + 1}`])) {
+      return grid[`${row - 1}:${column + 1}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row}:${column - 1}`])) {
+      return grid[`${row}:${column - 1}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row}:${column + 1}`])) {
+      return grid[`${row}:${column + 1}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row + 1}:${column - 1}`])) {
+      return grid[`${row + 1}:${column - 1}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row + 1}:${column}`])) {
+      return grid[`${row + 1}:${column}`];
+    }
+
+    if (this.rockPaperScissors(current_color, grid[`${row + 1}:${column + 1}`])) {
+      return grid[`${row + 1}:${column + 1}`];
+    }
+
+    return current_color;
+  }
+
+  getUpdate(algo) {
+    let updated_grid = {};
+
+    for (let i = 1; i <= algo.rows; i++) {
+      for (let j = 1; j <= algo.columns; j++) {
+        updated_grid[`${i}:${j}`] = this.checkNeighbors(i, j, algo.grid);
+      }
+    }
+    algo.grid = updated_grid;
+    return algo;
+  }
+
+}
+
 class GameOfLife {
+  getAlgo(rows, columns, initialConditions, runs) {
+    let algo = {};
+    algo["rows"] = rows;
+    algo["columns"] = columns;
+    algo["grid"] = {};
+    algo["runs"] = runs;
+    for (let index in initialConditions) {
+      algo.grid[initialConditions[index]] = "black";
+    }
+    
+    return algo;
+  }
+
   countNeighbors(row, column, grid) {
     let count = 0;
 
@@ -123,35 +197,76 @@ class GameOfLife {
     return count;
   }
 
-  getUpdate(rows, columns, grid) {
+  getUpdate(algo) {
     let updated_grid = {};
 
-    for (let i = 1; i <= rows; i++) {
-      for (let j = 1; j <= columns; j++) {
-        let neighborCount = this.countNeighbors(i, j, grid);
-        let isAlive = `${i}:${j}` in grid;
+    for (let i = 1; i <= algo.rows; i++) {
+      for (let j = 1; j <= algo.columns; j++) {
+        let neighborCount = this.countNeighbors(i, j, algo.grid);
+        let isAlive = `${i}:${j}` in algo.grid;
 
         if ((isAlive && (neighborCount === 2 || neighborCount === 3)) || (!isAlive && neighborCount === 3)) {
           updated_grid[`${i}:${j}`] = "black";
         }
       }
     }
-    return updated_grid;
+    algo.grid = updated_grid;
+    return algo;
   }
 }
 
-let gameOfLife = new GameOfLife();
-drawGrid();
-setCell(51, 51, "black");
-setCell(52, 51, "black");
-setCell(53, 51, "black");
-setCell(51, 52, "black");
-setCell(52, 50, "black");
-drawAutomota();
+let doGameOfLife = () => {
+  let gameOfLife = new GameOfLife();
+  let algo = gameOfLife.getAlgo(rows, columns, [`51:51`,`52:51`,`53:51`,`51:52`,`52:50`], 1500);
+  drawGrid(algo);
+  drawAutomota(algo.grid);
+  let loopId = setInterval(() => {
+    algo = gameOfLife.getUpdate(algo);
+    clearCanvas();
+    drawGrid(algo);
+    drawAutomota(algo.grid);
+    algo.runs = algo.runs - 1;
+    if (algo.runs <= 0) {
+      clearInterval(algo.loop);
+    }
+  }, 80);
+  algo["loop"] = loopId;
+}
 
-setInterval(() => {
-  grid_data = gameOfLife.getUpdate(rows, columns, grid_data);
-  clearCanvas();
-  drawGrid();
-  drawAutomota();
-}, 80);
+let doRockPaperScissors = () => {
+  let rockPaperScissors = new RockPaperScissors();
+  let initialConditions = [];
+
+  // For horizontal stripes, pretty boring
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= columns; j++) {
+      let color = "";
+      if ((i + j) % 3 === 0) {
+        color = "green";
+      } else if ((i + j) % 2 === 0) {
+        color = "red";
+      } else {
+        color = "blue";
+      }
+      initialConditions.push({ coordinates: `${i}:${j}`, color });
+    }
+  }
+  let algo = rockPaperScissors.getAlgo(rows, columns, initialConditions, 100);
+  drawGrid(algo);
+  drawAutomota(algo.grid);
+
+  let loopId = setInterval(() => {
+    algo = rockPaperScissors.getUpdate(algo);
+    clearCanvas();
+    drawGrid(algo);
+    drawAutomota(algo.grid);
+    algo.runs = algo.runs - 1;
+    if (algo.runs <= 0) {
+      clearInterval(algo.loop);
+    }
+  }, 80);
+  algo["loop"] = loopId;
+}
+
+// doGameOfLife();
+doRockPaperScissors();
